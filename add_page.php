8fc36ee805abd4226de2b46d250ada909ea52152
Andrey4090 <128154608+Andrey4090@users.noreply.github.com>
2024-03-21 11:05:03 +0300

    add scripts

--- add_page.php
+++ add_page.php
@@ -1,3 +1,18 @@
 <?php include './modules/header.php'; ?>
 <?php include './modules/add_obj.php'; ?>
+<?php 
+session_start();
+if (isset($_SESSION['message']) && $_SESSION['message'])
+{
+  echo $_SESSION['message_stats'], $_SESSION['message'],'</p>';
+  unset($_SESSION['message']);
+  unset($_SESSION['message_stats']);
+}?>
+<form  class="form_db" method="POST" action="./modules/file_upload.php" enctype="multipart/form-data">
+    <div>
+      <span>Загрузить файл:</span>
+      <input type="file" name="uploadedFile" required/>
+    </div>
+    <button type="submit" name="uploadBtn" value="Upload">Загрузить файл</button>
+  </form>  
 <?php include './modules/footer.php'; ?>
--- show_page.php
+++ show_page.php
@@ -1,3 +1,13 @@
 <?php include './modules/header.php'; ?>
 <?php include './modules/show_obj.php'; ?>
+<script>
+    // Generate QR code for each row in the table
+    var rows = document.querySelectorAll('tbody tr');
+    rows.forEach(function (row) {
+        var text = row.cells[0].innerText + ' - ' + row.cells[1].innerText + ' - ' + row.cells[2].innerText+ ' - ' + row.cells[3].innerText; // Combine cell values
+        var qrCode = document.createElement('img');
+        qrCode.src = 'https://api.qrserver.com/v1/create-qr-code/?size=100x100&data=' + text;
+        row.appendChild(qrCode);
+    });
+</script>
 <?php include './modules/footer.php'; ?>
